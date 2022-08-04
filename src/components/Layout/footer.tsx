965dd704ef2ab8093852b7eb9ab2436a989f0f58
Denis Didenko <didenkodenis1404@gmail.com>
2022-08-04 19:21:46 +0300

    fix lowercase

---
+++ src/components/Layout/footer.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Footer: FC = () => {
+    return (
+        <footer>
+            <div className='copyright'>Copyright</div>
+        </footer>
+    );
+};
+
+export default Footer;
